9d9af4072bfd3a97866feb384a3d85497eef41a8
Farid <fariedomar43@gmail.com>
2023-10-04 17:41:16 +0530

    first commit

--- graphql/resolvers/orderRsolver.js
+++ graphql/resolvers/orderRsolver.js
@@ -44,7 +44,7 @@ module.exports = {
               payment_method_types: ['card'],
               line_items: lineItems,
               mode: 'payment',
-              success_url: `${process.env.BASE_URL}/success`,
+              success_url: `${process.env.BASE_URL}/success-payment`,
               cancel_url: `${process.env.BASE_URL}`,
             });
 
