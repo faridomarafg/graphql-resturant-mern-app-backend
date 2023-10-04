eb0b1a672113dd61436bb5e2f7a36822fa0fbdc2
Farid <fariedomar43@gmail.com>
2023-10-04 18:41:44 +0530

    first commit

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ dbConnect();
 
 const app = express();
 
+app.use(bodyParser.json({limit:'30mb', extended: true}));
+
+app.use(bodyParser.urlencoded({limit:'30mb', extended: true}));
+
 app.use(
   cors({
     origin: ["http://localhost:3000", 'https://graphql-resturant-mern-app-frontend.onrender.com'],
